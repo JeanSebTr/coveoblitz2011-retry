1fcd668b681c6abd295fc72ecde036f81ae1f208
Jean-Sébastien Tremblay <jean-seb@total-itech.com>
2012-01-06 15:05:26 -0500

    mock des api :D

--- application/routes/api.js
+++ application/routes/api.js
@@ -3,24 +3,41 @@
  */
 
 module.exports = function(app, engine){
-    app.get('/api/search', function(req, res){
-        res.render('index.html', { title : 'Home page with an incredible title'});
-    });
-    
-    app.get('/api/index', function(req, res){
+   app.get('/api/search', function(req, res){
+      res.json({
+         'success': true,
+         'result': [
+            {
+               'title':'Ton titre bidon',
+               'summary':'lorem ipsum doloris amet blabla'
+            },
+            {
+               'title':'Ton title deux',
+               'summary':'lorem fidouda ipsumum'
+            }
+         ]
+      }, 200);
+   });
+   
+   app.get('/api/index', function(req, res){
       res.json({
          'success': true,
          'result': engine.progress
       }, 200);
-    });
-    
-    app.get('/api/tags', function(req, res){
-        res.render('index.html', { title : 'Home page with an incredible title'});
-    });
-
-    app.get('/test', function(req,res){
-        var jsonStr = '{"title": "Un titre"}';
-        var json = JSON.parse(jsonStr);
-        res.send(json.title);
-    });
+   });
+   
+   app.get('/api/tags', function(req, res){
+      res.json({
+         'success': true,
+         'result': {
+            'tata':20,
+            'toto':18,
+            'blob':22,
+            'beer':15,
+            'deer':24,
+            'nodejs':25,
+            'coveo':21
+         }
+      }, 200);
+   });
 };
