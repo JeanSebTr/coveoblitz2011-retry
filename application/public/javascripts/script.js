14c607dcdd2a41c325a90d08b7f19560dfded314
Jean-Philippe Poulin de Courval <jeep87c@Jean-Philippes-MacBook-Pro.local>
2012-01-06 18:25:11 -0500

    Correction bug positionnement du footer

--- application/public/javascripts/script.js
+++ application/public/javascripts/script.js
@@ -1,18 +1,1 @@
-$(function(){
-	positionFooter(); 
-	function positionFooter(){
-		var padding_top = $("footer").css("padding-top").replace("px", "");
-		var page_height = $(document.body).height() - padding_top + 10;
-		var window_height = $(window).height();
-		var difference = window_height - page_height;
-		if (difference < 0) 
-			difference = 0;
- 
-		$("footer").css({
-			padding: difference + "px 0 0 0"
-		})
-	}
- 
-	$(window)
-		.resize(positionFooter)
-});
+
