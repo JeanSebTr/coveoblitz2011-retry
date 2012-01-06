c7df5b0021341094121cf3692efaa382b7804191
jack_dbernier <jdesjardinsbernier@gmail.com>
2012-01-06 09:44:20 -0500

    routes in a dif file

--- app.js
+++ app.js
@@ -28,9 +28,8 @@ app.configure('production', function(){
   app.use(express.errorHandler()); 
 });
 
-// Routes
-
-app.get('/', routes.index);
+//Routing client
+require('./routes/default.js')(app);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
